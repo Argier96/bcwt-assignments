db4c62e0b1cd69e3ac4c72f592a3fcd6069aef86
riteshg <riteshg@metropolia.fi>
2022-10-27 21:19:09 +0300

    lodash works and npm start script as well. Done with this exercise

---
+++ week1-node/script.js
@@ -0,0 +1,9 @@
+'require strict'
+
+const _ = require("lodash")
+
+var result = _.camelCase("Foo Bar")
+console.log(result)
+
+var result2 = _.camelCase("This actually works.")
+console.log(result2)
