1bf3663afef2a537a051c7381a2e842317d7554f
riteshg <riteshg@metropolia.fi>
2022-11-01 14:59:53 +0200

    added get,put,post,delete route for app.js

--- week2/week2-starter/app.js
+++ week2/week2-starter/app.js
@@ -6,9 +6,23 @@ const port = 3000;
 app.get('/cat', (req, res) => {
   res.send('From this endpoint you can get cats.')
 });
-
+app.get('/cat/:catId', (req, res) => {
+  console.log(req.params)
+  res.send('From this endpoint you can get cats info with id: '+ req.params.catId)
+});
 app.get('/user', (req, res) => {
   res.send('From this endpoint you can get users.')
 });
 
+app.post('/cat', (req, res) => {
+  res.send('From this endpoint you can add more cats.')
+})
+
+app.put('/cat', (req, res) => {
+  res.send('From this endpoint you can update cats.')
+})
+
+app.delete('/cat', (req, res) => {
+  res.send('From this endpoint you can delete cats.')
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
