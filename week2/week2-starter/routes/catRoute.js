017a38879c45270297979c2963b126386fed7e5a
riteshg <riteshg@metropolia.fi>
2022-11-01 15:17:25 +0200

    routes defined at catRoutes.js

--- week2/week2-starter/routes/catRoute.js
+++ week2/week2-starter/routes/catRoute.js
@@ -1,2 +1,25 @@
 'use strict';
 // catRoute
+const express = require('express');
+const router = express.Router();
+
+router.get('/cat', (req, res) => {
+    res.send('From this endpoint you can get cats.')
+  });
+  router.get('/cat/:catId', (req, res) => {
+    console.log(req.params)
+    res.send('From this endpoint you can get cats info with id: '+ req.params.catId)
+  });
+  router.post('/cat', (req, res) => {
+    res.send('From this endpoint you can add more cats.')
+  })
+  
+  router.put('/cat', (req, res) => {
+    res.send('From this endpoint you can update cats.')
+  })
+  
+  router.delete('/cat', (req, res) => {
+    res.send('From this endpoint you can delete cats.')
+  })
+
+  module.exports = router;
